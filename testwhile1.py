353c36b6f024996eeafff545f0acd2f48ba4526a
movemountains <movemountains4321@gmail.com>
2014-09-03 11:02:11 -0700

    print number in backwards

---
+++ testwhile1.py
@@ -0,0 +1,20 @@
+a=21
+b=0
+
+while a>b:
+    #print(a)
+    a-=1
+
+    if a == 15:
+        print(a)
+        a -= 2
+    
+    print(a)
+
+    if a == 11:
+       print(a)
+       print('halfway done')   
+       a-=2
+ 
+    if a == 5:
+        a-=1     
