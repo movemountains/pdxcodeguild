5d1e887eb587167db64e8425295e6c7bb5944b5b
movemountains <movemountains4321@gmail.com>
2014-09-22 12:04:30 -0700

    unit test on vacation

---
+++ vacation_unit_test.py
@@ -0,0 +1,26 @@
+__author__ = 'kalyani'
+import unittest
+from vacation import hotel_cost
+from vacation import rental_car_cost
+
+
+
+class vacationTestcase(unittest.TestCase):
+
+    """test for hotel_cost"""
+
+
+    def test_hotel_cost(self):
+        self.assertTrue(hotel_cost(3.5))
+        assert isinstance(hotel_cost, int)
+
+
+    def test_rental_car_cost(self):
+        self.assertTrue(rental_car_cost("r"))
+        assert isinstance(rental_car_cost, int)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
