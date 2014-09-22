6c4b99fa8f82236e1f6d6b6c2ef77101a041aaf4
movemountains <movemountains4321@gmail.com>
2014-09-22 12:54:36 -0700

    test Phone_directory

---
+++ phone_directory_test.py
@@ -0,0 +1,18 @@
+
+__author__ = 'kalyani'
+
+import unittest
+import phone_directory
+import contact_dict
+
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def add_entry(self):
+        self.assertEqual(self.first_name,20)
+
+if __name__ == '__main__':
+    unittest.main()
